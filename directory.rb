32e1058750d1c45abf74b99a5393e1a897bd4229
Alexy <alexandra.fina@yahoo.com>
2017-12-12 20:41:31 +0000

    Printing with index

--- directory.rb
+++ directory.rb
@@ -36,7 +36,7 @@ puts "-------------"
 end
 def print(students)
 students.each_with_index do |student, index|
-  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chars.first == "A"
+  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
 end
 end
 def print_footer(names)
