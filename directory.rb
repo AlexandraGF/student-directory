edac32e8a6fc594313442b2febab84c6d6fd5388
Alexy <alexandra.fina@yahoo.com>
2017-12-14 19:44:26 +0000

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,30 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    puts "Choose an option:"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -29,6 +56,7 @@ def input_students
   students
 end
 
+=begin
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -42,6 +70,8 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
+
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
@@ -62,6 +92,7 @@ else
 end
 end
 
+interactive_menu
 students = input_students
 print_header
 print(students)
