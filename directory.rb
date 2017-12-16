603e660a8db1bbeda64edf048caba3732cadc378
Alexy <alexandra.fina@yahoo.com>
2017-12-16 11:03:02 +0000

    Refactoring and adding feedback message to the user

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
 @students = []
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "Press '1' to type in the students"
+  puts "Press '2' to show the students"
+  puts "Press '3' to save the list to students.csv file"
+  puts "Press '4' to load the list from students.csv file"
+  puts "Press '5' when you want to leave the program"
 end
 
 def interactive_menu
@@ -17,27 +17,19 @@ end
 
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit
-  when "3"
-    save_students
-  when "4"
-    load_students
+  when "1" then puts "You have chosen to type in students"; input_students
+  when "2" then puts "You have chosen to show the students"; show_students
+  when "3" then puts "You have chosen to save to the file"; save_students
+  when "4" then puts "You have chosen to load from the file"; load_student
+  when "5" then puts "You have chosen to leave the program"; exit
   else
     puts "I don't know what you meant, try again"
   end
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
+  puts "Please enter the names of the students\nTo finish, just hit return twice"
   name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
@@ -47,6 +39,7 @@ def input_students
   end
 end
 
+
 def show_students
   print_header
   print_student_list
@@ -54,14 +47,11 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy\n-------------"
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  @students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer
@@ -70,18 +60,13 @@ end
 
 def save_students
   file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
+  @students.each {|student| file.puts ([student[:name], student[:cohort]].join(","))}
   file.close
 end
 
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
+  file.readlines.each do |line| name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
@@ -95,7 +80,7 @@ def try_load_students
      puts "Loaded #{@students.count} from #{filename}"
   else
     puts "Sorry, #{filename} doesn't exist."
-    exit 
+    exit
   end
 end
 
